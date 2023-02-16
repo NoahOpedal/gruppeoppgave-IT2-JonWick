96be74c3d29c31bef923df7e9803004b9491d580
NoahOpedal <nopedal@gmail.com>
2023-02-16 10:44:22 +0100

    Made outline for key class

--- classes/key.js
+++ classes/key.js
@@ -1,3 +1,34 @@
 class Key{
-    #number; 
+    #number;
+    #collected;
+    #sprite;
+    #x;
+    #y;
+
+    constructor(number, sprite, x, y){
+        this.#number = number;
+        this.#sprite = sprite;
+        this.#collected = false;
+        this.#x = x;
+        this.#y = y;
+
+    }
+
+    get number(){
+        return this.#number;
+    }
+    get collected(){
+        return this.#collected;
+    }
+    get position(){
+        //return new Vektor2(x, y)
+    }
+
+    collect(){
+        this.#collected = true;
+    }
+
+    draw(){
+
+    }
 }
