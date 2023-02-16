1ebd95a271f01719468f38687e77f56c9ff174ad
AllackDev <andynesse04@gmail.com>
2023-02-16 10:46:32 +0100

    link te classes i html og vector class

---
+++ classes/vector.js
@@ -0,0 +1,66 @@
+class Vector {
+
+    #x;
+    #y;
+
+    constructor(x, y, polar=false) {
+        if (!polar) {
+            this.#x = x;
+            this.#y = y;
+        } else {
+            this.#x = x * Math.cos( y );
+            this.#y = x * Math.sin( y );
+        }
+    }
+
+    get x() {
+        return this.#x;
+    }
+
+    get y() {
+        return this.#y;
+    }
+
+    get length() {
+        let l = (this.#x**2 + this.#y**2)**(1/2);
+        return l;
+    }
+
+    get angle() {
+        return 0; // to-do
+    }
+
+    toString() {
+        let s = "[" + this.#x + ", " + this.#y + "]";
+        return s;
+    }
+
+    add(vector) {
+        this.#x += vector.x;
+        this.#y += vector.y;
+    }
+
+    subtract(vector) {
+        this.#x -= vector.x;
+        this.#y -= vector.y;
+    }
+
+    static add(left, right) {
+        let x = left.x + right.x;
+        let y = left.y + right.y;
+        return new Vector(x, y);
+    }
+
+    static subtract(left, right) {
+        let x = left.x - right.x;
+        let y = left.y - right.y;
+        return new Vector(x, y);
+    }
+
+    static random() {
+        let angle = Math.random() * 2 * Math.PI;
+        let x = Math.cos(angle);
+        let y = Math.sin(angle);
+        return new Vector(x,y);
+    }
+}
