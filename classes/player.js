6af11a881f2e32ca4115779b0bb628a1a64d3465
AllackDev <andynesse04@gmail.com>
2023-02-17 13:11:16 +0100

    iebcencenejc

--- classes/player.js
+++ classes/player.js
@@ -16,8 +16,8 @@ class Player{
         return new Vector(this.#position.x, this.#position.y);
     }
     get center(){
-        let x = this.#position.x + tilePixels/2;
-        let y = this.#position.y + tilePixels;
+        let x = this.#position.x + tileSize/2;
+        let y = this.#position.y + tileSize;
         return new Vector(x, y);
     }
 
@@ -32,17 +32,17 @@ class Player{
             this.#position.add(new Vector(0,1));
         }
         if (keys.w == true){
-            this.#velocity = new Vector(0,-1.5*tilePixels);
+            this.#velocity = new Vector(0,-1.5*tileSize);
             console.log(this.#velocity);
             keys.w = false
         }
-        this.#position.add(Vector.multiply(this.#velocity,tilePixels/60));
-        this.#velocity.add(Vector.multiply(new Vector(0,2),tilePixels/60));
+        this.#position.add(Vector.multiply(this.#velocity,tileSize/60));
+        this.#velocity.add(Vector.multiply(new Vector(0,2),tileSize/60));
     }
     draw(){
         ctx.beginPath();
         ctx.fillStyle = "blue";
-        ctx.fillRect(this.position.x,this.position.y, tilePixels, 2*tilePixels);
+        ctx.fillRect(this.position.x,this.position.y, tileSize, 2*tileSize);
         ctx.fill();
 
 
