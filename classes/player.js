b24234c7c1f1bab01f9157b42bc9bfc9eef2b59c
NoahOpedal <nopedal@gmail.com>
2023-02-23 10:10:33 +0100

    loaded player jumping sprite

--- classes/player.js
+++ classes/player.js
@@ -1,14 +1,17 @@
 class Player{
     #position;
     #velocity;
-    #lives;
-    #sprite;
+    #lives;    
 
-    constructor(x, y, lives, sprite){
+    constructor(x, y, lives){
+        let assetsLoaded = 0;
+        let totalAssets = 1;    //Husk å oppdatere denne når du legger til flere bildefiler
+        let allAssetsLoaded = false;
         this.#position = new Vector(x, y);
         this.#velocity = new Vector(0,0);
-        this.#lives = lives;
-        this.#sprite = sprite;
+        this.#lives = lives;        
+        let playerJumpingSprite = new Image();
+        imgSpriteSheet.src = "player_jumping_sprite.png";
 
     }
 
@@ -21,28 +24,43 @@ class Player{
         return new Vector(x, y);
     }
 
-    update(){
-        if (keys.a == true){
-            this.#position.subtract(new Vector(1,0));
+    loaded(){
+        assetsLoaded++;
+        if(assetsLoaded >= totalAssets){
+            allAssetsLoaded = true;
         }
-        if (keys.d == true){
-            this.#position.add(new Vector(1,0));
-        }
-        if (keys.s == true){
-            this.#position.add(new Vector(0,1));
-        }
-        if (keys.w == true){
-            this.#velocity = new Vector(0,-1.5*tileSize);
-            console.log(this.#velocity);
-            keys.w = false
-        }
-        this.#position.add(Vector.multiply(this.#velocity,tileSize/60));
-        this.#velocity.add(Vector.multiply(new Vector(0,2),tileSize/60));
     }
+
+    update(){
+        if(allAssetsLoaded){
+            if (keys.a == true){
+                this.#position.subtract(new Vector(1,0));
+            }
+            if (keys.d == true){
+                this.#position.add(new Vector(1,0));
+            }
+            if (keys.s == true){
+                this.#position.add(new Vector(0,1));
+            }
+            if (keys.w == true){
+                this.#velocity = new Vector(0,-1.5*tileSize);
+                console.log(this.#velocity);
+                keys.w = false
+            }
+            this.#position.add(Vector.multiply(this.#velocity,tileSize/60));
+            this.#velocity.add(Vector.multiply(new Vector(0,2),tileSize/60));
+        }    
+    }
+
     draw(){
-        ctx.beginPath();
-        ctx.fillStyle = "blue";
-        ctx.fillRect(this.position.x,this.position.y, tileSize, 2*tileSize);
-        ctx.fill();
+        if(allAssetsLoaded){
+            ctx.beginPath();
+            ctx.fillStyle = "blue";
+            ctx.fillRect(this.position.x,this.position.y, tileSize, 2*tileSize);
+            ctx.fill();
+            if(this.#velocity.y != 0){            
+
+        }
+        }
     }
 }
