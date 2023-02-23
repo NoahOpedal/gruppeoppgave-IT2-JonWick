b9ad3774cb3fb2145aac9f66a1034d152df83d8c
NoahOpedal <nopedal@gmail.com>
2023-02-23 10:55:46 +0100

    Changed main file locations

--- code.js
+++ code.js
@@ -14,35 +14,33 @@ const width = canvas.width = height*2;
 const tileSize = width/90;
 const pixel = tileSize/16;
 
+let fps = 60;
+let player = new Player(30, 30, 5);
+
 //Import sprites
 let totalAssets = 5; //Oppdater denne når vi legger til flere assets
 let assetsLoaded = 0;
-
 let playerCrouchingSprite = new Image();
-playerCrouchingSprite.src = "player_crouching_sprite.png";
+playerCrouchingSprite.src = "sprites/player_crouching_sprite.png";
 playerCrouchingSprite.onload = loaded();
 let playerIdleAnimationSheet = new Image();
-playerIdleAnimationSheet.src = "player_idle_animation.png";
+playerIdleAnimationSheet.src = "sprites/player_idle_animation.png";
 playerIdleAnimationSheet.onload = loaded();
 let playerJumpingSprite = new Image();
-playerJumpingSprite.src = "player_jumping_sprite.png";
+playerJumpingSprite.src = "sprites/player_jumping_sprite.png";
 playerJumpingSprite.onload = loaded();
 let playerWalkingLeftAnimationSheet = new Image();
-playerWalkingLeftAnimationSheet.src = "player_walking_left_animation.png";
+playerWalkingLeftAnimationSheet.src = "sprites/player_walking_left_animation.png";
 playerWalkingLeftAnimationSheet.onload = loaded();
 let playerWalkingRightAnimationSheet = new Image();
-playerWalkingRightAnimationSheet.src = "Player_walking_right_animation.png";
+playerWalkingRightAnimationSheet.src = "sprites/player_walking_right_animation.png";
 playerCrouchingSprite.onload = loaded();
-
-let player = new Player(30, 30, 5);
 
 function gameLoop(){
     ctx.clearRect(0, 0, width, height);
     player.update();
     player.draw();
 }
-
-let fps = 60;
 
 function loaded(){    
     assetsLoaded++;
