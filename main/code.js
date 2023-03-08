7d0b565aa2a44858cedc6912dff664686c57dee8
samuelvasshus <samuel.vasshus@gmail.com>
2023-03-08 13:07:54 +0100

    Collision

--- main/code.js
+++ main/code.js
@@ -1,6 +1,23 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+let roomTileValues = new Array(45);
+
+for(let i = 0; i < 45; i ++){
+    roomTileValues[i] = new Array(90);
+    for(let a = 0; a < 90; a++){
+
+        if (i == 45 ){
+            roomTileValues[i][a] = 1
+        }
+        else{
+            roomTileValues[i][a] = 0; 
+        }
+    }
+}
+
+console.log(roomTileValues);
+
 //Constants
 let keys = {
     a:false,
@@ -15,7 +32,7 @@ const tileSize = width/90;
 const pixel = tileSize/16;
 
 
-let player = new Player(30, 30, 5, "");
+let player = new Player(30, 30, 5, "", roomTileValues);
 
 function gameLoop(){
     ctx.clearRect(0, 0, width, height);
